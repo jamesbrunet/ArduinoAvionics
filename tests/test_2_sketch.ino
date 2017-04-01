06f6c349a73d465263bb309d9b3bebeaadfbe222
The-Renaissance <nashesvobodnoye@hotmail.com>
2017-04-01 15:16:22 -0400

    Revert "FIX TYPO"
    This reverts commit 9f22709efb39d5bfaae782548b7f883f7da1df47.

--- tests/test_2_sketch.ino
+++ tests/test_2_sketch.ino
@@ -29,9 +29,9 @@ void setup() {
   loadSequence(0,&abort_test);
   set_condition(ROCKET_ABORT,EQUAL,0);
   loadSequence(1,&launch_print);
-  set_condition(&getTimer,0,EQUAL,1);
+  set_condition(&getTimer,0,EQUAL,0);
   loadSequence(2,ROCKET_ABORT);
-  set_condition(&getTimer,10000,EQUAL,2);
+  set_condition(&getTimer,10000,EQUAL,0);
 }
 
 void loop() {
