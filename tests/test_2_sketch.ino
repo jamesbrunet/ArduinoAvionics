3a2168ef6fddb9bc3c379e1983b67ac8840b2c09
The-Renaissance <nashesvobodnoye@hotmail.com>
2017-04-04 01:19:29 -0400

    Error fixing
    Error fixing

--- tests/test_2_sketch.ino
+++ tests/test_2_sketch.ino
@@ -29,9 +29,9 @@ void setup() {
   loadSequence(0,&abort_test);
   set_condition(ROCKET_ABORT,EQUAL,0);
   loadSequence(1,&launch_print);
-  set_condition(&getTimer,0,EQUAL,0);
+  set_condition(&getTimer,0,EQUAL,1);
   loadSequence(2,ROCKET_ABORT);
-  set_condition(&getTimer,10000,EQUAL,0);
+  set_condition(&getTimer,10000,EQUAL,2);
 }
 
 void loop() {
