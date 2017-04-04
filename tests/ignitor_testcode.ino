2d5757dac63a8ec6a9e628a1ddee6f289d0ce272
The-Renaissance <nashesvobodnoye@hotmail.com>
2017-04-04 00:24:37 -0400

    Ignitor test code added
    Ignitor test code added (provided that we can build up a circuit in time)

---
+++ tests/ignitor_testcode.ino
@@ -0,0 +1,35 @@
+#include <ROCKET_SEQUENCER.h>
+
+const int SWITCH_PIN = 15; //the pin that starts the timer
+const int TIME = 0; // countdown time in seconds
+const int FIRE_DURATION = 1000; /*the time duration in which 
+                                 *the fire pin is set on HIGH
+                                 *(in milliseconds)
+                                 */
+const int FIRE_PIN = 17;// the pin that sends the firing signal to the relay
+// All numbers used to initialize those variables are arbitary.
+
+void fireStart(){
+  digitalWrite(FIRE_PIN,HIGH);
+  Serial.println("Relay circuit activated");
+}
+
+void fireStop(){
+  digitalWrite(FIRE_PIN,LOW);
+  Serial.println("Relay circuit deactivated");
+}
+
+void setup(){
+  Serial.begin(9600);
+  initialize(2,SWITCH_PIN);
+  pinMode(FIRE_PIN,OUTPUT);
+  setCountdownTime(0);
+  loadSequence(0,&fireStart);
+  set_condition(&getTimer,TIME * 1000,EQUAL,0);
+  loadSequence(1,&fireStop);
+  set_condition(&getTimer,(TIME*1000+FIRE_DURATION),EQUAL,1);
+}
+
+void loop(){
+  start();
+}
